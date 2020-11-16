f01da1743aaed3448dfbc304a0cb1fd482147fda
seikaku <amir12102009@gmail.com>
2020-11-17 00:09:54 +0800

    alerts and keyboard dismiss

--- App.js
+++ App.js
@@ -1,32 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, View, Text } from 'react-native';
-import Header from './components/header'
+import { FlatList, StyleSheet, View, Text, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
 
 export default function App() {
 
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1'},
     { text: 'create an app', key: '2'},
-    { text: 'play on the switch', key: '3'}
+    { text: 'play the switch', key: '3'}
   ]);
 
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key !== key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ]
+      });
+    } else {
+      Alert.alert('OOPS!', 'Todos must be 4 chars long', [
+        { text: 'Understood', onPress: () => console.log('alert closed') }
+      ])
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        {/* to Form */}
-        <View style={styles.list}>
-          <FlatList 
-            data={todos}
-            renderItem={({ item }) => (
-              <Text>{ item.text }</Text>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
+        <StatusBar style="auto" />
       </View>
-      <StatusBar style="auto" />
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
